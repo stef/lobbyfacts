c5e3b24c4a403f5265bd86ad51d98a3e6d1c3e45
stf <s@ctrlc.hu>
2015-10-22 22:41:33 +0200

    [enh] support previous directors + better org resolution

--- lobbyfacts/data/extract/meetings.py
+++ lobbyfacts/data/extract/meetings.py
@@ -50,6 +50,88 @@ def fetch(url, retries=5, ignore=[], params=None):
     f = fetch_raw(url, retries, ignore, params)
     return fromstring(f)
 
+host2portfolio = { u"President Jean-Claude Juncker" : "Presidency",
+                   u"Commissioner Günther Oettinger":"Digital Economy",
+                   u"Commissioner Miguel Arias Cañete":"Climate & Energy",
+                   u"Commissioner Cecilia Malmström":"Trade",
+                   u"Commissioner Jonathan Hill":"Financial Markets",
+                   u"Commissioner Johannes Hahn":"Neighbourhood",
+                   u"Vice-President Jyrki Katainen" : "Jobs & Growth",
+                   u"Commissioner Christos Stylianides":"Humanitarian Aid",
+                   u"Commissioner Corina Crețu":"Regional Policy",
+                   u"Commissioner Dimitris Avramopoulos":"Home Affairs",
+                   u"First Vice-President Frans Timmermans":"Better Regulation",
+                   u"Commissioner Phil Hogan":"Agriculture",
+                   u"Commissioner Violeta Bulc":"Transport",
+                   u"Commissioner Elżbieta Bieńkowska": "Internal Market",
+                   u"Vice-President Kristalina Georgieva": "Budget",
+                   u"Vice-President Andrus Ansip":"Digital Single Market",
+                   u"Vice-President Maroš Šefčovič":"Energy Union",
+                   u"Commissioner Tibor Navracsics":"Education",
+                   u"Commissioner Karmenu Vella":"Environment",
+                   u"High Representative / Vice-President Federica Mogherini":"External Action",
+                   u"Commissioner Vytenis Andriukaitis":"Health",
+                   u"Commissioner Neven Mimica":"Development",
+                   u"Vice-President Valdis Dombrovskis":"Euro",
+                   u"Commissioner Pierre Moscovici":"Economics & Tax",
+                   u"Commissioner Věra Jourová":"Justice",
+                   u"Commissioner Marianne Thyssen":"Employment",
+                   u"Commissioner Carlos Moedas":"Research",
+                   u"Commissioner Margrethe Vestager":"Competition",
+}
+
+prefix = 'Meetings of '
+prefix2 = 'Cabinet members of '
+postfix = ' with organisations and self-employed individuals'
+urlroot = 'http://ec.europa.eu/transparencyinitiative/meetings/'
+
+def get_urls():
+    for _, url in uuids:
+        root = fetch(url)
+
+        title = unws(' '.join(root.xpath('//h3//text()')))[len(prefix):-len(postfix)]
+        crs = root.xpath('//a[contains(@class,"breadcrumb-segment-last")]//text()')
+        if crs[0]=='The Commissioners':
+            if title.startswith(prefix2):
+                org = host2portfolio[title[len(prefix2):]]
+            else:
+                org = host2portfolio[title]
+        elif crs[0]=='DGs':
+            org = crs[1]
+            if org == 'Secretariat-General > What we do':
+                org = 'Secretariat-General'
+        else:
+            raise ValueError("uknown crs[0]")
+        #print ('\t'.join((url, org, title))).encode('utf8')
+        yield (url, org, title)
+
+        predurl = root.xpath('//a[text()="List of predecessors"]')
+        if len(predurl)==1:
+            # get predecessors urls
+            predroot = fetch(urljoin(url, predurl[0].get('href')))
+            for pred in predroot.xpath('//div[@id="titlerefpage"]/following-sibling::a'):
+                if pred.xpath('./text()')[0].startswith('Back to '):
+                    break
+                url = "%s%s" % (urlroot, pred.get('href'))
+                predroot=fetch(url)
+                title = unws(' '.join(predroot.xpath('//h3//text()')))[len(prefix):-len(postfix)]
+                crs = predroot.xpath('//a[contains(@class,"breadcrumb-segment-last")]//text()')
+                if crs[0]=='The Commissioners':
+                    if title.startswith(prefix2):
+                        org = host2portfolio[title[len(prefix2):]]
+                    else:
+                        org = host2portfolio[title]
+                elif crs[0]=='DGs':
+                    org = crs[1]
+                    if org == 'Secretariat-General > What we do':
+                        org = 'Secretariat-General'
+                else:
+                    raise ValueError("uknown crs[0]")
+                #print ('\t'.join((url, org, title))).encode('utf8')
+                yield (url, org, title)
+        elif len(predurl)>1:
+            print >>sys.stderr, "wtf", predurl
+
 def get_ents(nodes):
     ents = []
     for lm in nodes:
@@ -70,7 +152,7 @@ def get_ents(nodes):
         ents.append((name, tregid))
     return ents
 
-def scrape(url, title):
+def scrape(url, title, org):
     while True:
         try: root = fetch(url)
         except:
@@ -85,14 +167,12 @@ def scrape(url, title):
                 location = unws(fields[2].xpath('.//text()'))
                 entities = get_ents(fields[3])
                 subject = unws(fields[4].xpath('.//text()'))
-                org = ''.join(("\t",title)).strip()
             elif len(fields) == 4:
                 name = unws(title)
                 date = unws(fields[0].xpath('.//text()'))
                 location = unws(fields[1].xpath('.//text()'))
                 entities = get_ents(fields[2])
                 subject = unws(fields[3].xpath('.//text()'))
-                org = ''
             else:
                 print >>sys.stderr, "fields not 4-5, wtf"
                 raise
@@ -124,8 +204,8 @@ def extract(engine):
         pass
 
     i=0
-    for title, url in uuids:
-        for meeting in scrape(url, title):
+    for url, org, title in get_urls():
+        for meeting in scrape(url, title, org):
             sl.upsert(engine, table, meeting, ['meetid', 'identification_code'])
             i+=1
             if i % 100 == 0:
